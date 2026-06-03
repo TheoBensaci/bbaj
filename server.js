02e652053b98358291918427c12d6657f120e3db
Maxime Regenass <maxime.regenass@hes-so.ch>
2026-06-03 15:19:46 +0200

    mangodb

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express';
 import expressWs from 'express-ws';
 
+const { MongoClient } = require("mongodb");
+
 const app = express();
 expressWs(app);
 
@@ -8,11 +10,22 @@ let idRoom = 0;
 let idSocket = 0;
 const rooms = {};
 
+//Connexion db
+const client = new MongoClient("mongodb://localhost:27017");
+await client.connect();
+let db = client.db("baj");
+console.log("MongoDB connecté");
+
+
 // Serve the public directory
 app.use(express.static('app'));
 
 // Serve the src directory
 app.use('/src', express.static('src'));
+
+app.post('/publishMap', (req, res) => {
+    db.collection("map").insertOne(req.params.map);
+});
 
 app.post('/createRoom', (req, res) => {
     rooms[idRoom++] = { levelId: req.params.levelId, playersInfo: [] };
@@ -20,9 +33,10 @@ app.post('/createRoom', (req, res) => {
 
 // Websocket game events
 app.ws('/', (ws) => {
+    ws.room = null;
+    ws.id = idSocket++; 
+    
     ws.on('message', (msg) => {
-        ws.room = null;
-        ws.id = idSocket++;
         const data = JSON.parse(msg);
 
         if (data.type === 'join') {
@@ -37,7 +51,7 @@ app.ws('/', (ws) => {
                 username.trim().length === 0 ||
                 !/^[a-zA-Z0-9_-]{3,16}$/.test(username)
             ) {
-                //ws.sen() error le username pas valide TODO methode send
+                //ws.send() error le username pas valide TODO methode send
                 return;
             }
 
@@ -55,11 +69,23 @@ app.ws('/', (ws) => {
     });
 
     ws.on('close', (msg) => {
-        const data = JSON.parse(msg);
         rooms[ws.room].playersInfo[ws.id].socket = null;
     });
 });
 
-app.listen(3000);
 
-console.log('App started.');
+const server = app.listen(3000, () => {
+    console.log("App started");
+});
+
+
+//Quand on ferme le server, on ferme la connexion db
+async function shutdown() {
+    server.close(async () => {
+        await client.close();
+        process.exit(0);
+    });
+}
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
